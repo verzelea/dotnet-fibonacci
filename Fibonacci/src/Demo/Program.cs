09b7f09ae775f13dcfe38e3668fd72b76d3704f6
verzelea <arthurverzele@hotmail.com>
2021-10-04 14:11:51 +0200

    update

---
+++ Fibonacci/src/Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Diagnostics;
+
+Stopwatch stopwatch = new();
+stopwatch.Start();
+var tasks = await Fibonacci.Compute.ExecuteAsync(args);
+
+foreach (var task in tasks) Console.WriteLine($"Fibo result: {task}");
+stopwatch.Stop();
+Console.WriteLine($"{stopwatch.Elapsed.Seconds} s");
---
+++ Fibonacci/src/Fibonacci/Compute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Compute
+    {
+
+        public static async Task<List<int>> ExecuteAsync(string[] arguments)
+        {
+            var results = new List<int>();
+            var tasks = RunFibonaccis(arguments);
+            foreach (var task in tasks)
+            {
+                var resultAwaited = await task;
+                results.Add(resultAwaited);
+            }
+            return results;
+        }
+        
+        private static int Fib(int i) =>
+            i switch
+            {
+                int when i <= 2 => 1,
+                _ =>  Fib(i - 2) + Fib(i - 1)
+            };
+        
+        private static List<Task<int>> RunFibonaccis(string[] strings)
+        {
+            var list = new List<Task<int>>();
+            foreach (var input in strings)
+            {
+                var task = Task.Run(() => Fib(Convert.ToInt32(input)));
+                list.Add(task);
+            }
+
+            return list;
+        }
+    }
+}
